bcebcb0bc4b90beeaf5324f599aadc094bee562f
Khunt Miral <20ce051.miral.khunt@vvpedulink.ac.in>
2022-11-01 11:50:32 +0530

    question 2 done

--- .idea/Question 2/Que2.java
+++ .idea/Question 2/Que2.java
@@ -1,5 +1,18 @@
+import java.util.Scanner;
+
+//  WAP to find whether string is palindrome or not.
+
+
 public class Que2 {
+    static Scanner sc = new Scanner(System.in);
     public static void main(String[] args) {
-        
+        System.out.println("Enter a String : ");
+        String str = sc.nextLine();
+        StringBuilder sb = new StringBuilder(str).reverse();
+        if(str.equals(sb.toString())) {
+            System.out.println("Palindorome");
+        }else {
+            System.out.println("Not Palindrome");
+        }
     }
 }
