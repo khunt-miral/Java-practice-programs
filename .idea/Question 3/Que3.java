1302646e29a52901c7ee656ea9399e62e50fff0c
Khunt Miral <20ce051.miral.khunt@vvpedulink.ac.in>
2022-11-13 21:08:54 +0530

    Que 3 done

--- .idea/Question 3/Que3.java
+++ .idea/Question 3/Que3.java
@@ -1,7 +1,37 @@
+import java.util.Scanner;
+
 // WAP to find a Factor of a given number (iterative and recursive)
 
 public class Que3 {
+    static Scanner sc = new Scanner(System.in);
+
     public static void main(String[] args) {
+        System.out.println("Enter a number to find factor of that : ");
+        int number = sc.nextInt();
+        System.out.println("Using iterative method");
+        findFactor(number);
+        System.out.println("\nUsing Recursion");
+        findFactorRecursive(number, 1);
+
+    }
+
+    // using iterative method
+    static void findFactor(int input) {
+        for (int i = 1; i <= input; i++) {
+            if (input % i == 0) {
+                System.out.print(i + " ");
+            }
+        }
+    }
+
+    //using recursive method
+    static void findFactorRecursive(int input, int increment) {
+        
+        if(increment <= input) {
+            if(input % increment == 0) System.out.print(increment + " ");
+            findFactorRecursive(input, increment + 1);
+        }
+
         
     }
 }
---
+++ .idea/Question 3/tempCodeRunnerFile.java
@@ -0,0 +1,9 @@
+static void findFactorRecursive(int input, int increment) {
+        
+    //     if(increment <= input) {
+    //         if(input % increment == 0) System.out.print(increment + " ");
+    //     }
+
+    //     findFactorRecursive(input, increment + 1);
+
+    // }
